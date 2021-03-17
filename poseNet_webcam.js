3f2400ca5e9cb127e0bfe64906174e578b4bbae5
Julia <56744410+JuliaHutchison@users.noreply.github.com>
2021-03-17 15:00:03 -0500

    blah

--- poseNet_webcam.js
+++ poseNet_webcam.js
@@ -5,6 +5,8 @@ let noseX = 0;
 let noseY= 0;
 let eyerX= 0; //right eye
 let eyerY= 0;
+let eyelX =0; //left eye
+let eyely =0;
 
 function setup() {
   createCanvas(640, 480);
@@ -24,15 +26,17 @@ function draw() {
   
   let d = dist(noseX, noseY, eyerX, eyerY); //makes stay rationed in the Z direction
    //dist calculates the distance between the values u input using this equation d=√((x_2-x_1)²+(y_2-y_1)²)
-  console.log(d);
+  //console.log(d);
   //making the filter will make it into buttons so will have to use document .getlelement stuff and inside functions,
   //and withing them if then statements within them
   
-  //the filter 
+  //the stuff that will go on the face
   fill(255,0,0); 
   ellipse(noseX, noseY, d); //(x,y,width) nose :)
   fill(0,0,255);
   ellipse(eyerX,eyerY, d); // right eye:)
+  fill(0,0,255);
+  ellipse(eyelX,eyelY, d)
 }
 
 function gotPoses(poses){
@@ -40,12 +44,16 @@ function gotPoses(poses){
    //person 1 (aka 0 cuz its programming)-> the pose of that person-> and within that which body part-> position of the body part -> and then we want the x or y value
     let nX = poses[0].pose.keypoints[0].position.x; 
     let nY = poses[0].pose.keypoints[0].position.y;
-    let eX = poses[0].pose.keypoints[1].position.x;
-    let eY = poses[0].pose.keypoints[1].position.y;
+    let erX = poses[0].pose.keypoints[1].position.x;
+    let erY = poses[0].pose.keypoints[1].position.y;
+    let elX = poses[0].pose.keypoints[2].position.x;
+    let elY = poses[0].pose.keypoints[2].position.y;
     noseX = lerp(noseX, nX, 0.5);
     noseY = lerp(noseY, nY, 0.5);
-    eyerX = lerp(eyerX, eX, 0.5);
-    eyerY = lerp(eyerY, eY, 0.5);
+    eyerX = lerp(eyerX, erX, 0.5);
+    eyerY = lerp(eyerY, erY, 0.5);
+    eyelX = lerp(eyelX, elX, 0.5);
+    eyelY = lerp(eyelY, elY, 0.5);
   }
 }
   
