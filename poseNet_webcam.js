3e3e56a3c3eaf45d239c5e2c1300bac332c6d0b6
Julia <56744410+JuliaHutchison@users.noreply.github.com>
2021-03-17 13:12:28 -0500

    Updated code for filters
    I did not need to track the body parts so i got rid of them

--- poseNet_webcam.js
+++ poseNet_webcam.js
@@ -1,68 +1,50 @@
-
-let webcam_output;
 let poseNet;
-let poses = [];
+let video;
+ //setting certain bodypart values to 0
+let noseX = 0;
+let noseY= 0;
+let eyerX= 0; //right eye
+let eyerY= 0;
 
 function setup() {
-
   createCanvas(640, 480);
-  webcam_output = createCapture(VIDEO);
-  webcam_output.size(width, height);
-
-  poseNet = ml5.poseNet(webcam_output, modelReady);
-  poseNet.on('pose', function(results) {
-    poses = results;
-  });
-  webcam_output.hide();
-}
-function modelReady() {
-  select('#status').html('Model Loaded');
+  video = createCapture(VIDEO); //sets the camera up but doesnt' show it yet that happens in the draw function
+  video.hide();// without this their will be 2 displays of the video
+  poseNet = ml5.poseNet(video, modelReady); // checks if the cloud server is ready for poses
+  poseNet.on('pose', gotPoses);
 }
 
+function modelReady(){
+  console.log('model ready'); //lets yk in the console if the cloud server is ready for poses
 
 function draw() {
+  image(video,0,0);
+  
+  let d = dist(noseX, noseY, eyerX, eyerX); //makes stay rationed in the Z direction
+   //dist calculates the distance between the values u input using this equation d=√((x_2-x_1)²+(y_2-y_1)²)
+  console.log(d);
+  //making the filter will make it into buttons so will have to use document .getlelement stuff and inside functions,
+  //and withing them if then statements within them
+  
+  //the filter 
+  fill(255,0,0); 
+  ellipse(noseX, noseY, d); //(x,y,width) nose :)
+  fill(0,0,255);
+  ellipse(eyerX,eyerY, d); // right eye:)
+  
 
-  image(webcam_output, 0, 0, width, height);
-
-  drawKeypoints();
-  drawSkeleton();
 }
 
-function drawKeypoints(){
-
-  for (let i = 0; i < poses.length; i++) {
-    // For each pose detected, loop through all the keypoints
-    let pose = poses[i].pose;
-    for (let j = 0; j < pose.keypoints.length; j++) {
-      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-      let keypoint = pose.keypoints[j];
-      // Only draw an ellipse if the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
-        // choosing colour. RGB where each colour ranges from 0 255
-        fill(0, 0, 255);
-        // disable drawing outline
-        noStroke();
-        
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-      }
-    }
+function gotPoses(poses){
+  if (poses.length > 0) {
+    let nX = poses[0].pose.keypoints[0].position.x;
+    let nY = poses[0].pose.keypoints[0].position.y;
+    let eX = poses[0].pose.keypoints[1].position.x;
+    let eY = poses[0].pose.keypoints[1].position.y;
+    noseX = lerp(noseX, nX, 0.5);
+    noseY = lerp(noseY, nY, 0.5);
+    eyelX = lerp(eyelX, eX, 0.5);
+    eyelY = lerp(eyelY, eY, 0.5);
   }
 }
-
-function drawSkeleton() {
-
-  for (let i = 0; i < poses.length; i++) {
-    let skeleton = poses[i].skeleton;
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < skeleton.length; j++) {
-      // line start point
-      let startPoint = skeleton[j][0];
-      // line end point
-      let endPoint = skeleton[j][1];
-      // Sets the color used to draw lines and borders around shapes
-      stroke(0, 255, 0);
-      
-      line(startPoint.position.x, startPoint.position.y, endPoint.position.x, endPoint.position.y);
-    }
-  }
-}
+  
